814de2e3355b23235ce0da8c4bab1c4549f2fa80
lopes <lopes@arquia.com>
2020-05-06 16:38:19 -0300

    completed all CRUDs and their tests

--- src/app.js
+++ src/app.js
@@ -30,13 +30,54 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // const { id, title, url, techs } = request.body
-  // const { id } = request.params
+  const { title, url, techs } = request.body
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex( repository => repository.id === id);
+
+  if(repositoryIndex < 0){
+
+    return response.status(400).send();
+
+  }else{
+
+    const { likes } = repositories.find( repository => repository.id === id);
+    const repository = {
+      id,
+      title,
+      url,
+      techs,
+      likes
+    };
+
+    repositories[repositoryIndex] = repository;
+
+    return response.json(repository);
+
+  }  
+
+  
   
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex( repository => repository.id === id);
+
+  if(repositoryIndex < 0){
+
+     return response.status(400).send();
+
+    }else{
+
+      repositories.splice(repositoryIndex,1);
+
+      return response.status(204).send();
+
+    }
+  
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
